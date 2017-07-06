9a23369f4749b74cb193eee7b81fa0c3e50e488d
j- <j@skeoh.com>
2017-07-06 10:52:06 +1000

    Add root reducer unit test
    Fixes typings to tests can run.

---
+++ src/store/root.test.ts
@@ -0,0 +1,52 @@
+import rootReducer from './root';
+
+it('Can set foreground color', () => {
+	const state = {
+		foregroundColor: 'blue',
+		backgroundColor: 'white',
+	};
+	const action = {
+		type: 'SET_FOREGROUND',
+		payload: {
+			color: 'red',
+		},
+	};
+	const result = rootReducer(state, action);
+	expect(result).toEqual({
+		foregroundColor: 'red',
+		backgroundColor: 'white',
+	});
+});
+
+it('Can set background color', () => {
+	const state = {
+		foregroundColor: 'blue',
+		backgroundColor: 'white',
+	};
+	const action = {
+		type: 'SET_BACKGROUND',
+		payload: {
+			color: 'black',
+		},
+	};
+	const result = rootReducer(state, action);
+	expect(result).toEqual({
+		foregroundColor: 'blue',
+		backgroundColor: 'black',
+	});
+});
+
+it('Can switch foreground and background', () => {
+	const state = {
+		foregroundColor: 'black',
+		backgroundColor: 'white',
+	};
+	const action = {
+		type: 'SWITCH_FOREGROUND_BACKGROUND',
+	};
+	const result = rootReducer(state, action);
+	expect(result).toEqual({
+		foregroundColor: 'black',
+		backgroundColor: 'white',
+	});
+});
